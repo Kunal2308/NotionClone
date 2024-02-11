f845e6c73bc5ece267419c2a55a8b67915854896
Kunal2308 <kunalp2308@gmail.com>
2024-02-11 12:35:40 +0530

    Initial Set Up

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const Marketing = () => {
+  return (
+    <div>
+      <Button variant="outline">Click Me</Button>
+    </div>
+  );
+};
+
+export default Marketing;
